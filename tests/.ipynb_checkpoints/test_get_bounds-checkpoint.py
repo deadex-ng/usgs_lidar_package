94933b6a20639d2b70139d478792cd9d42d6aad8
deadex-ng <thisisfumba@gmail.com>
2021-08-21 06:51:30 +0000

    visualize tif

--- tests/.ipynb_checkpoints/test_get_bounds-checkpoint.py
+++ tests/.ipynb_checkpoints/test_get_bounds-checkpoint.py
@@ -10,7 +10,7 @@ class TestFileHandler(unittest.TestCase):
     def test_set_bounds(self):
         bounds = Bounds()
         actual = bounds.set_bounds(-93.756155, 41.918015, -93.747334, 41.921429)
-        expected = ([-10436887.43333523,-10435905.484106943],[5148706.389047224,5149217.145836504])
+        expected = (([-10436887.43333523,-10435905.484106943],[5148706.389047224,5149217.145836504]))
         self.assertEqual(actual, expected)
         
 
